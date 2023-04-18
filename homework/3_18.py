d7901eee408d11e2ec1a42871d6a5b8daee7315d
Zo PHPCoder99 <gergenkopzoya@gmail.com>
2023-04-18 17:33:17 +0700

    fixed 3_18

--- homework/3_18.py
+++ homework/3_18.py
@@ -1,10 +1,12 @@
-array = [1, 2, 3, 3, 4, 6, 1]
+array = [1, 2, 4, 6, 1]
+array.sort()
 x = 3
 result = 0
 min_diff = abs(array[0]-x)
 
 for i in range(len(array)):
     if abs(array[i]-x) < min_diff:
+        min_diff = abs(array[i]-x)
         result = array[i]
 
 print(result)
